d3e48003b3633ef0d6b4c82bea664202a87d1237
Amit Kumar Verma <121687719+amitkumarverma01@users.noreply.github.com>
2025-08-25 23:15:14 +0530

    Update provider.tf

--- Environment/Dev/provider.tf
+++ Environment/Dev/provider.tf
@@ -5,15 +5,16 @@ terraform {
       version = "4.14.0"
     }
   }
-  backend "azurerm" {
-    
-  }
 
+  backend "azurerm" {
+    resource_group_name   = "jarvis"
+    storage_account_name  = "jarvisstorageaccount001"
+    container_name        = "jarvis"
+    key                   = "vinod.tfstate"
+  }
 }
 
 provider "azurerm" {
-  # Configuration options
   features {}
-  
   subscription_id = "177b7e12-5f03-4f63-bcd1-ed6d1d776bff"
 }

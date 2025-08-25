5aa9b1b16745efca6bfbe0deee3b5013528d6bfc
akkc01 <amitkkc01@gmail.com>
2025-08-25 23:52:19 +0530

    add all codness deleted

--- module/public_ip/main.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_public_ip" "pip" {
-  name                = var.pip
-  resource_group_name = var.rg_name
-  location            = var.location
-  allocation_method   = "Static"
-  sku                 = "Basic"
-}

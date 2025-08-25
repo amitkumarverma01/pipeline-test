f53680430dcee39ca8c2a459c29b7c5e82ef9bfa
akkc01 <amitkkc01@gmail.com>
2025-08-25 23:26:06 +0530

    add all codes

--- module/public_ip/main.tf
+++ module/public_ip/main.tf
@@ -3,5 +3,5 @@ resource "azurerm_public_ip" "pip" {
   resource_group_name = var.rg_name
   location            = var.location
   allocation_method   = "Static"
-  sku = "Basic"
+  sku                 = "Basic"
 }
